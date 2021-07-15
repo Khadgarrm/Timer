7c7b27348644a5d664e5c8325916ed1341a0f4eb
Nikita <khadgararm@gmail.com>
2021-07-15 14:24:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const React = require('react')
+const ms = require('pretty-ms')
+
+
+class Timer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: 0,
+      start: 0,
+      isOn: false
+    }
+    this.startTimer = this.startTimer.bind(this)
+    this.stopTimer = this.stopTimer.bind(this)
+    this.waitTimer = this.waitTimer.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
+  }
+  startTimer() {
+    this.setState({
+      time: this.state.time,
+      start: Date.now() - this.state.time,
+      isOn: true
+    })
+    this.timer = setInterval(() => this.setState({
+      time: Date.now() - this.state.start
+    }), 1000);
+  }
+  stopTimer() {
+    this.setState({isOn: false})
+    clearInterval(this.timer)
+  }
+  waitTimer() {
+    this.setState({isOn: false})
+    clearInterval(this.timer)
+  }
+  resetTimer() {
+    this.setState({time: 0, isOn: false})
+  }
+  render() {
+    let start = (this.state.time === 0) ?
+    <Button variant="success" onClick={this.startTimer}>Start</Button> :
+      null
+
+    let stop = (this.state.time === 0 || !this.state.isOn) ? null :
+    <Button variant="danger" onClick={this.stopTimer}>Stop</Button>
+
+    let wait = (this.state.time === 0 || !this.state.isOn) ?
+      null :
+      <Button variant="light" ondblClick={this.stopTimer}>Wait</Button>
+
+    let reset = (this.state.time === 0 || this.state.isOn) ? null :
+    <Button variant="dark" onClick={this.resetTimer}>Reset</Button> 
+      
+
+     return (
+       <div>
+         <h3>timer: {ms(this.state.time)}</h3>
+         {start}
+         {wait}
+         {stop}
+         {reset}
+       </div>
+     );
+  }
 }
-
-export default App;
+export default Timer
